466043a790f1ac1c2117d19b217e0072d9a9233b
GodSamble <osgneb8359@naver.com>
2023-04-03 19:48:05 +0900

    day2

---
+++ day2_ii.py
@@ -0,0 +1,18 @@
+a_list=['A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O', 'AB', 'A', 'B', 'O']
+
+A_cnt=0
+B_cnt=0
+O_cnt=0
+AB_cnt=0
+
+for i in a_list:
+    if i == 'A':
+        A_cnt+=1
+    elif i == 'B':
+        B_cnt+=1
+    elif i =='O':
+        O_cnt+=1
+    else:
+        AB_cnt+=1
+
+print(A_cnt,B_cnt,O_cnt,AB_cnt)
